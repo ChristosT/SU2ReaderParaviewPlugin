7271ddef7a5b4c02bc4c8771463e27657f7fa6e7
Christos Tsolakis <ctsolakis@cs.odu.edu>
2018-10-10 15:21:34 -0400

    simplify templated recursion

--- SU2_mesh_io.h
+++ SU2_mesh_io.h
@@ -2,12 +2,9 @@
 #define SU2_MESH_IO_H
 
 #include <fstream>
-#include <functional>
-#include <tuple>
 namespace SU2_MESH_IO
 {
 
-enum class file_mode {READ,WRITE};
 enum SU2Keyword
 {
     NDIME,
@@ -25,68 +22,62 @@ enum SU2Keyword
     PRISM = 13,
     PYRAMID = 14
 };
+enum class file_mode {READ,WRITE};
 
 struct SU2_mesh
 {
-    int dim; // Mesh dimension
-    file_mode fmode; //
+    int dim;            // Mesh dimension
+    file_mode fmode;    
     std::fstream file;
-    int64_t KwLocations[NumKw] = {-1};
+    int64_t KwLocations[NumKw] = {-1}; // file offset from the beginning of the file
+                                       // where Kword blocks are located
     //TODO boolean flag wheter points have index or not
 
 };
 
 
 bool open_mesh(const char* filename, enum file_mode fmode, SU2_mesh& mesh);
+
 /**
  * Read number entities under kwd and set position to this kwd
  */
 int64_t stat_kwd(SU2_mesh& mesh, int kwd);
 
-// 3D points
-void get_line(SU2_mesh& mesh, SU2Keyword type, double& x, double& y, double& z, int64_t& index);
-// 2D point
-void get_line(SU2_mesh& mesh, SU2Keyword type, double& x, double& y, int64_t& index);
-// read typoe of next element
+/**
+ * Read type of next element
+ */
 SU2Keyword get_element_type(SU2_mesh& mesh);
 
-// This is a template used to help us check that all arguments of the variadic 
-// function are the same. It is evaluated recursively at compile time
-// source https://codereview.stackexchange.com/a/51533
-template <typename ... Types>
-struct template_all; // UNDEFINED
+/**
+ * Read next 3D point
+ */
+void get_line(SU2_mesh& mesh, SU2Keyword type, double& x, double& y, double& z, int64_t& index);
 
-// base case for recursion : zero argumets return true
-template <>
-struct template_all<> : std::true_type {};
+/**
+ * Read next 2D point
+ */
+void get_line(SU2_mesh& mesh, SU2Keyword type, double& x, double& y, int64_t& index);
 
-// a false case plus anything returns false
-template <typename ... Types>
-struct template_all<std::false_type, Types...> : std::false_type {};
+/** 
+ * Read next element connectivity
+ */
 
-// recursive step : a true case plus anything returns the result of the predicate
-// applied on the rest of the arguments
-template <typename ... Types>
-struct template_all<std::true_type, Types...> : template_all<Types...>::type {};
+// base case for compile-time recursion
+inline void get_line(SU2_mesh& mesh, SU2Keyword type) {};
 
-template<typename... Args>
-void get_line(SU2_mesh& mesh, SU2Keyword type, Args&... arguments)
+template<typename T, typename... Args>
+void get_line(SU2_mesh& mesh, SU2Keyword type, T& arg, Args&... arguments)
 {
-//    static_assert( template_all< typename std::is_same<Args,int64_t>::type... >::value,
-//                   "all arguments should be of type int64_t");
+    static_assert( std::is_integral<T>::value, 
+                     "all arguments should be of integral type");
 
     //TODO assert type matches number of elements
-    using T = typename std::tuple_element<0, std::tuple<Args...> >::type;
 
-    //std::initializer_list <std::reference_wrapper<mA>> vals = {arguments...};
-    std::reference_wrapper<T> vals[] = {arguments...};
-    //for(auto i : vals)
-    for(size_t i  = 0 ; i < sizeof...(arguments); i++)
-    {
-        //mesh.file >> i.get();
-        mesh.file >> vals[i].get();
-    }
+    mesh.file >> arg;
+
+    get_line(mesh,type,arguments...);
 }
+
 };
 
 #endif /* SU2_MESH_IO_H */
